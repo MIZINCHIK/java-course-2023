84233ac4e1400f4740ee09764d291e4b8dda6dd3
MIZINCHIK <mizinchik@yahoo.com>
2023-10-07 23:55:26 +0700

    Test Task8

---
+++ src/test/java/edu/hw1/Task8Test.java
@@ -0,0 +1,55 @@
+package edu.hw1;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static edu.hw1.Task8.knightBoardCapture;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+public class Task8Test {
+    @Test
+    @DisplayName("Board with no conflicts")
+    void firstBoard() {
+        assertThat(knightBoardCapture(new int[][] {{0, 0, 0, 1, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 1, 0, 0, 0, 1, 0, 0},
+            {0, 0, 0, 0, 1, 0, 1, 0},
+            {0, 1, 0, 0, 0, 1, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 1, 0, 0, 0, 0, 0, 1},
+            {0, 0, 0, 0, 1, 0, 0, 0}})).isTrue();
+    }
+
+    @Test
+    @DisplayName("Board with conflicts")
+    void secondBoard() {
+        assertThat(knightBoardCapture(new int[][] {{1, 0, 1, 0, 1, 0, 1, 0},
+            {0, 1, 0, 1, 0, 1, 0, 1},
+            {0, 0, 0, 0, 1, 0, 1, 0},
+            {0, 0, 1, 0, 0, 1, 0, 1},
+            {1, 0, 0, 0, 1, 0, 1, 0},
+            {0, 0, 0, 0, 0, 1, 0, 1},
+            {1, 0, 0, 0, 1, 0, 1, 0},
+            {0, 0, 0, 1, 0, 1, 0, 1}})).isFalse();
+    }
+
+    @Test
+    @DisplayName("Second board with conflicts")
+    void thirdBoard() {
+        assertThat(knightBoardCapture(new int[][] {{0, 0, 0, 0, 1, 0, 0, 0},
+            {0, 0, 0, 0, 0, 1, 0, 0},
+            {0, 0, 0, 1, 0, 0, 0, 0},
+            {1, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 1, 0, 0, 0},
+            {0, 0, 0, 0, 0, 1, 0, 0},
+            {0, 0, 0, 0, 0, 1, 0, 0},
+            {1, 0, 0, 0, 0, 0, 0, 0}})).isFalse();
+    }
+
+    @Test
+    @DisplayName("Not a chess board")
+    void incorrectBoard() {
+        assertThatThrownBy(() -> knightBoardCapture(new int[][] {{1, 1},
+            {1, 1}})).isInstanceOf(IllegalArgumentException.class);
+    }
+}
