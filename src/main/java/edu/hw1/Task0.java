30f6ddebf33e4f16b8291d6cf5af3b5de1ebf723
MIZINCHIK <mizinchik@yahoo.com>
2023-10-07 18:02:33 +0700

    Fix codestyle

--- src/main/java/edu/hw1/Task0.java
+++ src/main/java/edu/hw1/Task0.java
@@ -3,11 +3,11 @@ package edu.hw1;
 import org.apache.logging.log4j.LogManager;
 
 public class Task0 {
-    public static void helloWorld() {
-        LogManager.getLogger().info("Привет, мир!");
+    private Task0() {
+        throw new IllegalStateException();
     }
 
-    public static void main(String[] args) {
-        helloWorld();
+    public static void helloWorld() {
+        LogManager.getLogger().info("Привет, мир!");
     }
 }
--- src/main/java/edu/hw1/Task1.java
+++ src/main/java/edu/hw1/Task1.java
@@ -1,18 +1,26 @@
 package edu.hw1;
 
 public class Task1 {
+    private static final int MAX_SECONDS = 59;
+    private static final int MIN_SECONDS = 0;
+    private static final int SECONDS_IN_MINUTE = 60;
+
+    private Task1() {
+        throw new IllegalStateException();
+    }
+
     public static long minutesToSeconds(String mmSS) {
         String[] separated = mmSS.split(":");
         if (separated.length != 2 || separated[1].length() != 2) {
             return -1;
         }
         long seconds = Long.parseLong(separated[1]);
-        if (seconds >= 60 || seconds < 0) {
+        if (seconds > MAX_SECONDS || seconds < MIN_SECONDS) {
             return -1;
         }
         long minutes = Long.parseLong(separated[0]);
         boolean negative = minutes < 0;
-        seconds += Math.abs(minutes) * 60;
+        seconds += Math.abs(minutes) * SECONDS_IN_MINUTE;
         return negative ? -seconds : seconds;
     }
 }
