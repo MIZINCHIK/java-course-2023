dcca2ab4a3d0933faadf026db26e3dff03f6032d
MIZINCHIK <mizinchik@yahoo.com>
2023-10-08 00:02:58 +0700

    Fix codestyle

--- src/main/java/edu/hw1/Task8.java
+++ src/main/java/edu/hw1/Task8.java
@@ -1,7 +1,7 @@
 package edu.hw1;
 
 public class Task8 {
-    private static final int[][] possibleTurns = new int[][] {{2, 1}, {1, 2}, {-1, 2},
+    private static final int[][] POSSIBLE_TURNS = new int[][] {{2, 1}, {1, 2}, {-1, 2},
         {-2, 1}, {-2, -1}, {-1, -2}, {1, -2}, {2, -1}};
     private static final int BOARD_DIMENSIONS = 8;
 
@@ -18,10 +18,10 @@ public class Task8 {
                 if (chessBoard[row][column] != 1) {
                     continue;
                 }
-                for (int[] turn : possibleTurns) {
-                    if (turn[0] + row >= BOARD_DIMENSIONS ||
-                        turn[0] + row < 0 || turn[1] + column >= BOARD_DIMENSIONS ||
-                        turn[1] + column < 0) {
+                for (int[] turn : POSSIBLE_TURNS) {
+                    if (turn[0] + row >= BOARD_DIMENSIONS
+                        || turn[0] + row < 0 || turn[1] + column >= BOARD_DIMENSIONS
+                        || turn[1] + column < 0) {
                         continue;
                     }
                     if (chessBoard[row + turn[0]][column + turn[1]] == 1) {
