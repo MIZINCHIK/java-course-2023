61cff96ac4bd1893d43044e7a8fc3d7fd2f286fc
MIZINCHIK <mizinchik@yahoo.com>
2023-10-15 16:42:18 +0700

    Task1: Implement Exponent record
    Exponent is evaluated using the Math module. Evaluation on a incorrect numbers behaves the same way producing NaN on output.

---
+++ src/main/java/edu/hw2/Task1/Exponent.java
@@ -0,0 +1,26 @@
+package edu.hw2.Task1;
+
+public record Exponent(Expr base, Expr power) implements Expr {
+    public Exponent {
+        if (base == null || power == null) {
+            throw new IllegalArgumentException("Null expressions can't be evaluated");
+        }
+    }
+
+    public Exponent(double base, double power) {
+        this(new Constant(base), new Constant(power));
+    }
+
+    public Exponent(double base, Expr power) {
+        this(new Constant(base), power);
+    }
+
+    public Exponent(Expr base, double power) {
+        this(base, new Constant(power));
+    }
+
+    @Override
+    public double evaluate() {
+        return Math.pow(base.evaluate(), power.evaluate());
+    }
+}
