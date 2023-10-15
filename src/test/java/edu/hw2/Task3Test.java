ec3da69ba4c7d27f740f9cd2de3965568d1630aa
MIZINCHIK <mizinchik@yahoo.com>
2023-10-15 23:36:36 +0700

    Task3: Test exceptions in the solution

---
+++ src/test/java/edu/hw2/Task3Test.java
@@ -0,0 +1,22 @@
+package edu.hw2;
+
+import edu.hw2.Task3.Connection.ConnectionException;
+import edu.hw2.Task3.ConnectionManagement.FaultyConnectionManager;
+import edu.hw2.Task3.PopularCommandExecutor;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+public class Task3Test {
+    @Test
+    @DisplayName("Executor throws exception if the limit is exceeded")
+    void exceptionExecutor() {
+        var connectionManager = new FaultyConnectionManager();
+        var executor = new PopularCommandExecutor(connectionManager, 1);
+        for (int i = 0; i < 5; i++) {
+            executor.tryExecute("command");
+        }
+        assertThatThrownBy(() -> executor.tryExecute("command")).isInstanceOf(ConnectionException.class)
+            .hasCauseInstanceOf(ConnectionException.class);
+    }
+}
