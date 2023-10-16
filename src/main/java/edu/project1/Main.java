1ad187ea1b93543d913e47cdca3298c195b36d6b
MIZINCHIK <mizinchik@yahoo.com>
2023-10-16 18:33:44 +0700

    Implement an entry point for an example instance of the game

--- src/main/java/edu/project1/Main.java
+++ src/main/java/edu/project1/Main.java
@@ -1,27 +1,21 @@
 package edu.project1;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import edu.project1.Dictionary.Dictionary;
+import edu.project1.Dictionary.SetDictionary;
+import java.util.HashSet;
+import java.util.List;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public final class Main {
-    private final static Logger LOGGER = LogManager.getLogger();
+    private final static int MAX_MISTAKES = 3;
 
     private Main() {
+        throw new IllegalStateException();
     }
 
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        LOGGER.info("Hello and welcome!");
-
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 0; i <= 2; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            LOGGER.info("i = {}", i);
-        }
+        Dictionary dictionary =
+            new SetDictionary(new HashSet<>(List.of("get")));
+        Controller game = new Controller(dictionary, MAX_MISTAKES);
+        game.runGame();
     }
 }
