d7d75c02485f2d97a06a754159546148eccfdcc6
MIZINCHIK <mizinchik@yahoo.com>
2023-10-16 18:26:16 +0700

    Implement Dictionary interface & SetDictionary class implementing the former
    Dictionary can add & remove words. SetDictionary is based on a set provided in a constructor.

---
+++ src/main/java/edu/project1/Dictionary/Dictionary.java
@@ -0,0 +1,7 @@
+package edu.project1.Dictionary;
+
+public interface Dictionary {
+    String getWord();
+
+    boolean removeWord(String word);
+}
---
+++ src/main/java/edu/project1/Dictionary/SetDictionary.java
@@ -0,0 +1,27 @@
+package edu.project1.Dictionary;
+
+import org.jetbrains.annotations.NotNull;
+import java.util.Random;
+import java.util.Set;
+
+public class SetDictionary implements Dictionary {
+    private final Set<String> dictionary;
+    private final Random random = new Random();
+
+    public SetDictionary(@NotNull Set<String> dictionary) {
+        this.dictionary = dictionary;
+    }
+
+    @Override
+    public String getWord() {
+        if (dictionary.isEmpty()) {
+            return null;
+        }
+        return dictionary.stream().skip(new Random().nextInt(dictionary.size())).findFirst().orElse(null);
+    }
+
+    @Override
+    public boolean removeWord(String word) {
+        return dictionary.remove(word);
+    }
+}
