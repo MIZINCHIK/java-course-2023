0fb467b705d597d025fd1655166d12e376820560
MIZINCHIK <mizinchik@yahoo.com>
2023-10-21 18:43:33 +0700

    Task4: Change throwing a Throwable to constructing it

--- src/main/java/edu/hw2/task4/CallingInfo.java
+++ src/main/java/edu/hw2/task4/CallingInfo.java
@@ -2,12 +2,7 @@ package edu.hw2.task4;
 
 public record CallingInfo(String className, String methodName) {
     public static CallingInfo callingInfo() {
-        StackTraceElement lastCall;
-        try {
-            throw new Throwable();
-        } catch (Throwable e) {
-            lastCall = e.getStackTrace()[1];
-        }
+        StackTraceElement lastCall = new Throwable().getStackTrace()[1];
         return new CallingInfo(lastCall.getClassName(), lastCall.getMethodName());
     }
 }
