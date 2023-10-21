ae41147a630170fed67beb66b4fdcf075de2535e
MIZINCHIK <mizinchik@yahoo.com>
2023-10-21 20:07:05 +0700

    Controller: Fit to new view logic and state encapsualtion

--- src/main/java/edu/project1/Controller.java
+++ src/main/java/edu/project1/Controller.java
@@ -3,78 +3,54 @@ package edu.project1;
 import edu.project1.dictionary.Dictionary;
 import edu.project1.model.Model;
 import edu.project1.model.State;
+import edu.project1.view.HangmanView;
+import edu.project1.view.TerminalWriter;
+import static edu.project1.model.GuessResult.DEFEAT;
+import static edu.project1.model.GuessResult.WIN;
 import static edu.project1.util.TerminalReader.readMessage;
-import static edu.project1.util.TerminalWriter.greet;
-import static edu.project1.util.TerminalWriter.printAllowedMistakesAmount;
-import static edu.project1.util.TerminalWriter.printAnswer;
-import static edu.project1.util.TerminalWriter.printCorrectGuess;
-import static edu.project1.util.TerminalWriter.printCurrentWordMask;
-import static edu.project1.util.TerminalWriter.printDefeat;
-import static edu.project1.util.TerminalWriter.printError;
-import static edu.project1.util.TerminalWriter.printGiveUp;
-import static edu.project1.util.TerminalWriter.printMistakesLeft;
-import static edu.project1.util.TerminalWriter.printNewWordMask;
-import static edu.project1.util.TerminalWriter.printNoMoreWords;
-import static edu.project1.util.TerminalWriter.printRules;
-import static edu.project1.util.TerminalWriter.printWin;
-import static edu.project1.util.TerminalWriter.printWrongGuess;
 
 public class Controller {
-    private final static String SAFE_WORD = "give up";
+    private static final String SAFE_WORD = "give up";
+
     private final Model model;
     private final int maxMistakes;
+    private final HangmanView view;
 
     public Controller(Dictionary dictionary, int maxMistakes) {
         this.maxMistakes = maxMistakes;
         this.model = new Model(dictionary, maxMistakes);
+        this.view = new TerminalWriter();
     }
 
     public void runGame() {
-        greet();
-        printRules(SAFE_WORD);
-        printAllowedMistakesAmount(maxMistakes);
+        view.greet();
+        view.showRules(SAFE_WORD);
+        view.showAllowedMistakesAmount(maxMistakes);
         while (true) {
             if (!model.startNewGame()) {
-                printNoMoreWords();
+                view.showNoMoreWords();
                 return;
             }
-            printNewWordMask(model.getUserWordMask());
+            view.showNewWordMask(model.getUserWordMask());
             runGuessCycle();
         }
     }
 
     private void runGuessCycle() {
-        guessCycle:
         while (true) {
             String userInput = readMessage();
             if (userInputIsSafeWord(userInput)) {
-                printGiveUp();
+                view.showGiveUp();
                 System.exit(0);
             }
             if (!userInputIsCorrect(userInput)) {
-                printRules(SAFE_WORD);
+                view.showRules(SAFE_WORD);
                 continue;
             }
             State state = model.guess(userInput.charAt(0));
-            switch (state.result()) {
-                case WIN -> printWin();
-                case DEFEAT -> printDefeat();
-                case FAILED -> printWrongGuess();
-                case GUESSED -> printCorrectGuess();
-                default -> printError();
-            }
-            switch (state.result()) {
-                case WIN, DEFEAT -> {
-                    printAnswer(model.getCurrentWord());
-                    break guessCycle;
-                }
-                case FAILED, GUESSED -> {
-                    printCurrentWordMask(model.getUserWordMask());
-                    printMistakesLeft(model.getMistakesLeft());
-                }
-                default -> {
-                    break guessCycle;
-                }
+            view.showState(state);
+            if (state.result() == WIN || state.result() == DEFEAT) {
+                break;
             }
         }
     }
