6d519fcb59278ddc906348a3621bd0f6da45dd9c
MIZINCHIK <mizinchik@yahoo.com>
2023-10-28 16:23:08 +0700

    Task5: Change to implementing Comparable<Contact>

--- src/main/java/edu/hw3/task5/Contact.java
+++ src/main/java/edu/hw3/task5/Contact.java
@@ -1,25 +1,31 @@
 package edu.hw3.task5;
 
+import org.jetbrains.annotations.NotNull;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.Objects;
 
-public record Contact(String firstName, String lastName) {
+public record Contact(String firstName, String lastName) implements Comparable<Contact> {
     public static Contact[] parseContacts(String[] contacts, ContactSortingOrder order) {
         return Arrays.stream(contacts).filter(Objects::nonNull).map(contact -> {
             String[] names = contact.split(" ", 2);
             return new Contact(names[0], names.length >= 2 ? names[1] : null);
-        }).sorted((o1, o2) -> {
-            int compareValue;
-            if (o1.lastName == null && o2.lastName == null) {
-                compareValue = o1.firstName.compareTo(o2.firstName);
-            } else if (o1.lastName == null) {
-                compareValue = o1.firstName.compareTo(o2.lastName);
-            } else if (o2.lastName == null) {
-                compareValue = o1.lastName.compareTo(o2.firstName);
-            } else {
-                compareValue = o1.lastName.compareTo(o2.lastName);
-            }
-            return compareValue * order.getComparatorValue();
-        }).toArray(Contact[]::new);
+        }).sorted(order == ContactSortingOrder.DESC
+            ? Comparator.reverseOrder()
+            : Comparator.naturalOrder()).toArray(Contact[]::new);
+    }
+
+    @Override
+    public int compareTo(@NotNull Contact o) {
+        if (lastName == null && o.lastName == null) {
+            return firstName.compareTo(o.firstName);
+        }
+        if (lastName == null) {
+            return firstName.compareTo(o.lastName);
+        }
+        if (o.lastName == null) {
+            return lastName.compareTo(o.firstName);
+        }
+        return lastName.compareTo(o.lastName);
     }
 }
