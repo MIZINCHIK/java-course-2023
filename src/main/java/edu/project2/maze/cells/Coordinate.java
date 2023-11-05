67b525df4563cb2b63ba52efa0d8312ee1e020f4
MIZINCHIK <mizinchik@yahoo.com>
2023-11-05 14:10:13 +0700

    Cells: Add method to get all neighbours at straight lines with a step

--- src/main/java/edu/project2/maze/cells/Coordinate.java
+++ src/main/java/edu/project2/maze/cells/Coordinate.java
@@ -1,4 +1,15 @@
 package edu.project2.maze.cells;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public record Coordinate(int row, int column) {
+    public List<Coordinate> getNeighbours(int step) {
+        var result = new ArrayList<Coordinate>();
+        result.add(new Coordinate(row(), column() + step));
+        result.add(new Coordinate(row() + step, column()));
+        result.add(new Coordinate(row(), column() - step));
+        result.add(new Coordinate(row() - step, column()));
+        return result;
+    }
 }
