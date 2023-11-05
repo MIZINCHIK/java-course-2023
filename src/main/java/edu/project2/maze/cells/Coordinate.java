ec96df145b14b7b9adaaa51edf6bc50d3eb857a8
MIZINCHIK <mizinchik@yahoo.com>
2023-11-05 18:41:13 +0700

    Cell: Return neighbours in a Set

--- src/main/java/edu/project2/maze/cells/Coordinate.java
+++ src/main/java/edu/project2/maze/cells/Coordinate.java
@@ -1,11 +1,11 @@
 package edu.project2.maze.cells;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 public record Coordinate(int row, int column) {
-    public List<Coordinate> getNeighbours(int step) {
-        var result = new ArrayList<Coordinate>();
+    public Set<Coordinate> getNeighbours(int step) {
+        var result = new HashSet<Coordinate>();
         result.add(new Coordinate(row(), column() + step));
         result.add(new Coordinate(row() + step, column()));
         result.add(new Coordinate(row(), column() - step));
