c5b82ae0d99ecc2361eabf5b61e2bb7ef070443c
MIZINCHIK <mizinchik@yahoo.com>
2023-11-18 13:44:32 +0700

    Task4: Fix codestyle

--- src/main/java/edu/hw6/task4/StreamComposition.java
+++ src/main/java/edu/hw6/task4/StreamComposition.java
@@ -9,17 +9,23 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
 import java.util.zip.CRC32;
 import java.util.zip.CheckedOutputStream;
-
 import static java.nio.file.Files.newOutputStream;
 
 public class StreamComposition {
     private static final Path FILES = Path.of(System.getProperty("user.dir"))
         .resolve("src/test/resources/edu/hw6/task3");
 
+    private StreamComposition() {
+        throw new IllegalStateException();
+    }
+
     public static void writeComposeStreams(Path file) {
         try (
-            Writer writer = new PrintWriter(new OutputStreamWriter(new BufferedOutputStream(new CheckedOutputStream(newOutputStream(FILES.resolve(
-                file)), new CRC32())), StandardCharsets.UTF_8))) {
+            Writer writer = new PrintWriter(new OutputStreamWriter(new BufferedOutputStream(new CheckedOutputStream(
+                newOutputStream(FILES.resolve(
+                    file)),
+                new CRC32()
+            )), StandardCharsets.UTF_8))) {
             writer.write("Programming is learned by writing programs. ― Brian Kernighan");
         } catch (IOException e) {
             throw new RuntimeException(e);
