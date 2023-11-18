7d0920e0f75c997d381c67c278762f94c89e876d
MIZINCHIK <mizinchik@yahoo.com>
2023-11-18 13:42:19 +0700

    Task4: Compose Streams

---
+++ src/main/java/edu/hw6/task4/StreamComposition.java
@@ -0,0 +1,28 @@
+package edu.hw6.task4;
+
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.io.Writer;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Path;
+import java.util.zip.CRC32;
+import java.util.zip.CheckedOutputStream;
+
+import static java.nio.file.Files.newOutputStream;
+
+public class StreamComposition {
+    private static final Path FILES = Path.of(System.getProperty("user.dir"))
+        .resolve("src/test/resources/edu/hw6/task3");
+
+    public static void writeComposeStreams(Path file) {
+        try (
+            Writer writer = new PrintWriter(new OutputStreamWriter(new BufferedOutputStream(new CheckedOutputStream(newOutputStream(FILES.resolve(
+                file)), new CRC32())), StandardCharsets.UTF_8))) {
+            writer.write("Programming is learned by writing programs. ― Brian Kernighan");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
