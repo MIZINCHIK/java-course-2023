10c1e2dfc3230cd09e6cf8a88b11a67e7e452e26
MIZINCHIK <mizinchik@yahoo.com>
2023-11-20 23:42:54 +0700

    Task2: Solve

---
+++ src/main/java/edu/hw7/task2/MultithreadFactorial.java
@@ -0,0 +1,25 @@
+package edu.hw7.task2;
+
+import java.math.BigInteger;
+import java.util.NoSuchElementException;
+import java.util.stream.Stream;
+
+public class MultithreadFactorial {
+    public static final String NEGATIVE_ARGUMENT = "Operation not defined for negative numbers";
+
+    private MultithreadFactorial() {
+        throw new IllegalStateException();
+    }
+
+    public static BigInteger calculateFactorial(int number) throws IllegalArgumentException {
+        try {
+            return Stream.iterate(BigInteger.ONE, BigInteger.ONE::add)
+                .limit(number)
+                .parallel()
+                .reduce(BigInteger::multiply)
+                .orElse(BigInteger.ONE);
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException(NEGATIVE_ARGUMENT, e);
+        }
+    }
+}
